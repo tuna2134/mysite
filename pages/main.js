63530273362af26b4075f3e2e84e6769fe7571bb
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-20 08:31:06 +0900

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -2,14 +2,16 @@ import styles from '../styles/Home.module.css'
 
 export default function Main() {
   return (
-    <h1>tuna2134 offical site</h1>
+    <div className={styles.container}>
+      <h1>tuna2134 offical site</h1>
     
-    <div className={styles.grid}>
-      <h3>
-        RT bot
-      </h3>
+      <div className={styles.grid}>
+        <h3>
+          RT bot
+        </h3>
 
-      <a href="https://rt-bot.com">website</a>
+        <a href="https://rt-bot.com">website</a>
+      </div>
     </div>
   )
 }
