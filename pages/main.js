5e95fe6cb732bf033591764ab2da9e899f8fa86f
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-20 07:55:53 +0900

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -1,5 +1,10 @@
 export default function Main() {
   return (
     <h1>tuna2134 offical site</h1>
+    <h2>work</h2>
+    <div className={style.grid}>
+      <h3>RT bot</h3>
+      <a href="https://rt-bot.com">website</a>
+    </div>
   )
 }
