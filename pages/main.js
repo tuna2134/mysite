fb25f9f63ba7a466c05e74f2efbb11fe23cef018
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-20 22:23:54 +0900

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -13,7 +13,6 @@ export default function Main() {
       <div className={styles.grid}>
         <div className={styles.card}>
           <h3 className="text-lg">RT Bot</h3>
-
           <a href="https://rt-bot.com">website</a>
         </div>
         <div className={styles.card}>
@@ -42,6 +41,7 @@ export default function Main() {
       <div className={styles.grid}>
         <div className={styles.card}>
           <h3 className="text-lg">Python</h3>
+          <Image src="/F3503098-1F3C-4CCF-8955-16E2338541D4.jpeg" alt="Python image"/>
         </div>
         <div className={styles.card}>
           <h3 className="text-lg">Golf</h3>
